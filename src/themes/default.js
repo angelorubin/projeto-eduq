db775313889d99fe7231f44b7c71e5e702948c66
angelorubin <angelorubin@gmail.com>
2023-04-10 08:39:15 -0300

    Implementando breakpoints

--- src/themes/default.js
+++ src/themes/default.js
@@ -1,13 +1,21 @@
-import { createTheme } from '@mui/material';
+import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#1E88E5',
-      },
-      secondary: {
-        main: '#FFC107',
-      },
+  palette: {
+    primary: {
+      main: "#1E88E5",
     },
-  });
-  
+    secondary: {
+      main: "#FFC107",
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 400,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
